5ebcdf15ea32d3176a56d9db72f984c70e757652
Олександр Заставний <alex.roox5@gmail.com>
2020-05-20 10:45:13 +0300

    Loop change in transfer

--- views/services/transfers.blade.php
+++ views/services/transfers.blade.php
@@ -14,14 +14,14 @@
                 <h2 class="section-title">{!! get_field('countries_title') !!}</h2>
                 <div class="base-size mb-0 mb-md-5">(@trans('выберите страну из списка'))</div>
                 <div class="row selected-country sm-inline-blocks">
-                    <div class="col-lg-3 col-md-4">
-                        @php global $post  @endphp
-                        @foreach($ukraine as $post)
-                            @php $flag = get_field('country_from') == 'ua' ? 'uk.jpg' : 'ru.jpg' @endphp
-                            @include('services.includes.country')
-                        @endforeach
-                        @reset_query
-                    </div>
+					@php global $post  @endphp
+					@foreach($ukraine as $post)
+					<div class="col-lg-3 col-md-4">
+						@php $flag = get_field('country_from') == 'ua' ? 'uk.jpg' : 'ru.jpg' @endphp
+						@include('services.includes.country')
+					</div>
+					@endforeach
+					@reset_query
                 </div>
             </div>
         </section>
@@ -31,14 +31,14 @@
                 <h2 class="section-title">{!! get_field('countries_title_second') !!}</h2>
                 <div class="base-size mb-0 mb-md-5">(@trans('выберите страну из списка'))</div>
                 <div class="row selected-country sm-inline-blocks">
-                    <div class="col-lg-3 col-md-4">
                         @php global $post  @endphp
                         @foreach($russia as $post)
-                            @php $flag = get_field('country_from') == 'ua' ? 'uk.jpg' : 'ru.jpg' @endphp
-                            @include('services.includes.country')
+						<div class="col-lg-3 col-md-4">
+							@php $flag = get_field('country_from') == 'ua' ? 'uk.jpg' : 'ru.jpg' @endphp
+							@include('services.includes.country')
+						</div>
                         @endforeach
                         @reset_query
-                    </div>
                 </div>
             </div>
         </section>
